6251a3c16d98b0b9a16c1ae511735013c8a2886d
Vlad Crudu <56761465+vl-cr@users.noreply.github.com>
2025-01-02 17:15:12 +0000

    macos: Add window click feature and remove longpress from Obsidian

--- setup/macos.sh
+++ setup/macos.sh
@@ -2,13 +2,15 @@
 
 echo "macOS: changing OS defaults..."
 
-### Application Settings ###
-# VSCode key repeat: https://github.com/vscode-neovim/vscode-neovim?tab=readme-ov-file#troubleshooting
+# Disable key longpress in apps
+defaults write md.obsidian ApplePressAndHoldEnabled -bool false
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
-### Finder ###
-# Show hidden files
+# Finder: Show hidden files
 defaults write com.apple.finder AppleShowAllFiles -boolean true
 
+# Add cmd+ctrl+click window feature: https://mmazzarolo.com/blog/2022-04-16-drag-window-by-clicking-anywhere-on-macos
+defaults write -g NSWindowShouldDragOnGesture -bool true
+
 # If change to Brew zsh is needed on macOS: https://rick.cogley.info/post/use-homebrew-zsh-instead-of-the-osx-default
-echo "macOS: leaving pre-installed zsh as the default one (check if it's stale)"
+echo "macOS: Leaving pre-installed zsh as the default one (check if it's stale)"
