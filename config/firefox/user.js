387aed2a87ed04fa9126e6d9029968865bfa5f9f
Vlad Crudu <56761465+vl-cr@users.noreply.github.com>
2026-02-17 12:13:15 +0000

    firefox: Update profile config

--- config/firefox/user.js
+++ config/firefox/user.js
@@ -2,3 +2,5 @@ user_pref("full-screen-api.transition-duration.enter", "0 0");
 user_pref("full-screen-api.transition-duration.leave", "0 0");
 user_pref("sidebar.revamp.defaultLauncherVisible", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("security.enterprise_roots.enabled", true);
+
